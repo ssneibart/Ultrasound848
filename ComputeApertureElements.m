5553586c6c98f6cb907a905d0c8aea95ea34a4bd
Tyler Glass <tylerglass33@gmail.com>
2016-02-02 23:56:12 -0500

    Commenting aperture function

--- ComputeApertureElements.m
+++ ComputeApertureElements.m
@@ -1,9 +1,13 @@
 function [ IncludedApertureElements ] = ComputeApertureElements( LateralDistanceMatrix, FocusR, FNumb )
-%Computes which elements to include for each beam location
+%   Computes which elements to include for each beam location
 %   Lateral Distance Matrix = lateral distance of elements to beam location
 %   FocusR = receive focus
 %   FNumb = F number = focus/aperture
-aperture_size = FocusR/FNumb;
+%   IncludedApertureElements = logical matrix with values of 1 at rows
+%   corresponding to transducer elements that should be included in
+%   aperture at beam location specified by the column
+
+aperture_size = FocusR/FNumb; % F = z/D
 
 IncludedApertureElements = not(abs(sign(sign(-aperture_size/2 - LateralDistanceMatrix) + ...
     sign(aperture_size/2 - LateralDistanceMatrix))));
