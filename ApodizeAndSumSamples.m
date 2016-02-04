54925a0dd662ea2ca2c2d4fb9db85c4996b3c5bc
Tyler Glass <tylerglass33@gmail.com>
2016-02-04 15:05:14 -0500

    Function to apodize samples

---
+++ ApodizeAndSumSamples.m
@@ -0,0 +1,20 @@
+function [ rfData, ApodizationProfile ] = ApodizeAndSumSamples( DelayedCroppedSamples )
+%Applies apodization and summing to cropped and delayed samples
+
+%% Extract Data Information
+[NumSamp, NumEl, NumLines] = size(DelayedCroppedSamples);
+
+%% Create Apodization Profile Sin Wave
+t = 0.5:1:NumEl-0.5;
+A = 1; % Amplitude of apodization
+ApodizationProfile = A*sin(pi*t/NumEl); % half-sine wave with length equal to number of elements
+
+%% Apply Apodization to Samples
+ApodizationMatrix = repmat(ApodizationProfile,NumSamp,1,NumLines);
+ApodizedSamples   = ApodizationMatrix.*DelayedCroppedSamples;
+
+%% Sum Apodized Samples to get rfData
+rfData = reshape(sum(ApodizedSamples,2),NumSamp,NumLines);
+
+end
+
