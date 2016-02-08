c82c2fb73a68cb1eb140c7fc455d4aadd2f1a654
Tyler Glass <tylerglass33@gmail.com>
2016-02-08 14:48:44 -0500

    Added Sample indices for focus ranges

--- MultiFocusDelayCalculator.m
+++ MultiFocusDelayCalculator.m
@@ -1,4 +1,4 @@
-function [MultiFocusDistanceIndexMatrix, MultiFocusPosition_cm] = MultiFocusDelayCalculator(LateralDistanceMatrix, MultiFocusPositionFractions, NumbSamples, dx)
+function [MultiFocusDistanceIndexMatrix, MultiFocusPosition_cm, MultiFocusRangeSampleIndices] = MultiFocusDelayCalculator(LateralDistanceMatrix, MultiFocusPositionFractions, NumbSamples, dx)
 %DelayCalculator Calculates the Delays associated with each element
 %   MultiFocusPositions is a vector with % of where to focus on receive
 %   that is 5 elements long (e.g. [0.18 0.36 0.54 0.72 0.90] would focus at
@@ -7,6 +7,13 @@ function [MultiFocusDistanceIndexMatrix, MultiFocusPosition_cm] = MultiFocusDela
 
 %% Convert Focus Position Fractions to Distances in CM
 MultiFocusPosition_cm = MultiFocusPositionFractions*NumbSamples*dx/2; 
+
+%% Compute Ranges for Focus
+MultiFocusRange = zeros(1,length(MultiFocusPositionFractions)-1);
+for index = 1:length(MultiFocusPositionFractions)-1
+    MultiFocusRange(index)=mean([MultiFocusPositionFractions(index),MultiFocusPositionFractions(index+1)]);    
+end
+MultiFocusRangeSampleIndices = round(MultiFocusRange*NumbSamples);
 
 %% Loop through focus positions to find delays
 % preallocate size
