fdfff722db2a137f349ac4b1d63cce07c63709a2
Tyler Glass <tjg17@duke.edu>
2016-02-09 03:11:47 -0500

    Removed old data

--- data/readTransducerParams.m
+++
@@ -1,9 +0,0 @@
-function [ElementSpacing] = readTransducerParams()
-%readTransducerParams loads the parameters associated with the transducer
-%   Pretty much just that
-
-%% Element Spacing
-ElementSpacing = 0.0201; % cm
-
-end
-
