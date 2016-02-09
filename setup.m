0d41e7afed648254abcfb2a9de4d9905e5cdc15f
Tyler Glass <tylerglass33@gmail.com>
2016-02-09 16:55:38 -0500

    Change bf params

--- setup.m
+++ setup.m
@@ -1,3 +1,3 @@
 addpath(genpath('Code'))
 beamformer = @beamform_tjg17_ssn6_bz45_jko11;
-bf_params = struct('FocusFractions',[1,2,3,4,5]*0.18,'FNumb',30,'Gain',0.0015,'SampleFraction',0.9);
+bf_params = struct('FocusFractions',[1,2,3,4,5]*0.18,'FNumb',25,'Gain',0.0015,'SampleFraction',0.9);
