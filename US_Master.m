20aa7a9f088ea297214c62bcd59afa4ee0bd52f8
Tyler Glass <tjg17@duke.edu>
2016-02-09 22:10:47 -0500

    Readability for lab report

--- US_Master.m
+++ US_Master.m
@@ -1,95 +1,83 @@
 %% US Master Code
+% Single Receive Focus beamforming
 
 %% Start
-clear; %close all; clc;
+clear; clc %close all; clc;
 
 %% Load Data and Constants
 cd ./data/ % go into data directory
-[M,NumbLines,NumbElements,NumbSamples] = readBinData('imageData_PlaneWave.bin');
+% Choose Plane Wave for Focused Data
+%[M,NumLines,NumElements,NumSamples] = readBinData('imageData_PlaneWave.bin');
+[M,NumLines,NumElements,NumSamples] = readBinData('imageData_Focused.bin');
 [ElementSpacing] = readTransducerParams();
-[BeamSpacing, fs, FocusR, FocusT, FNumb, t0] = readImageParams();
+[BeamSpacing, fs, FocusR, FocusT, FNum, t0] = readImageParams();
 [c] = readConstants();
 cd .. % go back to coding directory
 
 %% Other Constants
-F_bf = 2.5; % cm
-bw = 0.55; 
-x = 0.6; % compressive value
-ReceiveAperture = FocusR/FNumb;
-ReceiveDepth = 3; % cm
+bw = 0.55;   % fractional bandwidth for BPF
+x = 0.6;     % compressive value
+fc = 4*10^6; % center frequency
+a = 0.0015;  % gain
 
 %% Time and Space Intervals
 dt = 1/fs; % s
 dx = c*dt; % cm 
 
-%% Other
-FocalIndex = FocusR./dx; % index
-
 %% Spatial Location Calculations
-[BeamLocations, ElementLocations, SampleLocations, SampleIndices] = SpatialLocator(BeamSpacing, NumbLines, ElementSpacing, NumbElements, dx, NumbSamples,t0);
+[BeamLocations, ElementLocations, SampleLocations, SampleIndices] = SpatialLocator(BeamSpacing, NumLines, ElementSpacing, NumElements, dx, NumSamples,t0);
 
 %% Delay Calculations
 [LateralDistanceMatrix, DistanceIndexMatrix] = DelayCalculator(BeamLocations, ElementLocations, FocusR,dx);
 
-%% Delay Calculations for 5 Foci
-MultiFocusPositionFractions = [0.18 0.36 0.54 0.72 0.90];
-[MultiFocusDistanceIndexMatrix, MultiFocusPosition_cm, MultiFocusRangeSampleIndices] = MultiFocusDelayCalculator(LateralDistanceMatrix, MultiFocusPositionFractions, NumbSamples, dx);
-
 %% Compute Center Elements in Aperture for All Beams
-[numElements_HalfAperture, CenterElementNum]  = ComputeApertureElements( LateralDistanceMatrix, FocusR, FNumb, ElementSpacing );
+[numElements_HalfAperture, CenterElementNum]  = ComputeApertureElements( LateralDistanceMatrix, FocusR, FNum, ElementSpacing );
 
-%% Delay and Crop Samples using Truncation and Include only Elements in Aperture
-DelayedCroppedSamples  = DelayandCropSamples( DistanceIndexMatrix, M, CenterElementNum, NumbSamples, numElements_HalfAperture, NumbLines );
+%% Choose Delay and Sum Method for Sections A, E, Extra Credit
+%Part A
+%Delay and Crop Samples using Truncation and Include only Elements in Aperture
+DelayedCroppedSamples  = DelayandCropSamples( DistanceIndexMatrix, M, CenterElementNum, NumSamples, numElements_HalfAperture, NumLines );
 
-%% Delay and Crop Samples for CenterLineData from Single Beam
+%Part E
+%Delay and Crop Samples for CenterLineData from Single Beam
+%DelayedCroppedSingleBeamSamples = DelayandCropSingleBeamSamples( DistanceIndexMatrix, M, CenterElementNum, NumSamples, numElements_HalfAperture, NumLines );
 
-%[ DelayedCroppedSingleBeamSamples ] = DelayandCropSingleBeamSamples_ApertureGrowthComp( DistanceIndexMatrix, M, NumbSamples, NumbLines, c, fs, LateralDistanceMatrix, FocusR, FNumb, ElementSpacing, dx);
-%[ DelayedCroppedSingleBeamSamples1 ] = DelayandCropSingleBeamSamples( DistanceIndexMatrix, M, CenterElementNum, NumbSamples, numElements_HalfAperture, NumbLines );
-
-%DelayedCroppedSingleBeamSamples = DelayandCropSingleBeamSamples( DistanceIndexMatrix, M, CenterElementNum, NumbSamples, numElements_HalfAperture, NumbLines );
-
-%% Delay and Crop for CenterlineData using Aperture Growth (Extra Credit)
-%DelayedCroppedSingleBeamApertureGrowthSamples = DelayandCropSingleBeamSamples_ApertureGrowthComp( DistanceIndexMatrix, M, NumbSamples, NumbLines, c, fs, LateralDistanceMatrix, FocusR, FNumb, ElementSpacing, dx);
-
-%% Delay and Crop for Multiple rxFocus
-%DelayedCroppedMultipleFocusSamples = DelayandCropMultipleFocusSamples(MultiFocusDistanceIndexMatrix,M, CenterElementNum, NumbSamples, numElements_HalfAperture, NumbLines, MultiFocusRangeSampleIndices );
+%Extra Credit
+%Delay and Crop for CenterlineData using Aperture Growth (Extra Credit)
+%DelayedCroppedSingleBeamApertureGrowthSamples = DelayandCropSingleBeamSamples_ApertureGrowthComp( DistanceIndexMatrix, M, NumSamples, NumLines, c, fs, LateralDistanceMatrix, FocusR, FNum, ElementSpacing, dx);
 
 %% Sum Samples Using Apodization
-rfData = ApodizeAndSumSamples( DelayedCroppedSamples );
-
-%% Constants for Image Generation
-fc = 4*10^6; 
-fs = 40*10^6;
+% Toggle apodization in ApodizeAndSumSamples.m function
+% Choose Delayed and Cropped Samples for A,E,Extra Credit as above
+[rfData, ApodizationProfile] = ApodizeAndSumSamples( DelayedCroppedSamples );
 
 %% Bandpass Filter 
 Filt = bandpassfilter(rfData, fc,fs);
 
 %% Time Gain Compensation ( gain as a function of time or depth)
-a = 0.0015;
 Filtamp = TGC(Filt, a);
 
 %% Demodulation (Envelope Detection)
 Envel = abs(hilbert(Filtamp));
 
-%% Log compression s that we can see the areas that we are looking at
-x =0.6;
+%% Log compression (fixes dynamic range)
 A = exp(x*log(Envel));
-% colorRange = [0 50];
 
-%% Ranges 
-lateralRange = NumbLines*BeamSpacing;
+%% Ranges (for plotting)
+lateralRange = NumLines*BeamSpacing;
 lateral = (-lateralRange/2:BeamSpacing:lateralRange/2)*10;
-
 axialRangetime = size(A,1)*dt;
 axialRangem = axialRangetime*c/2;
 axial  = (0:dt*c/2:axialRangem)*10; 
 
-
 %% Image Formation 
-figure
+figure(1)
 colormap(gray)
 imagesc(lateral,axial,A) % colorRange)
 ylabel('Axial (mm)')
 xlabel('Lateral (mm)')
 axis image
 
+%% Plot Apodization Profile
+figure(2)
+plot(ApodizationProfile)
--- multipleRX.m
+++ multipleRX.m
@@ -5,10 +5,12 @@ clear; clc
 
 %% Load Data and Constants
 cd ./data/ % go into data directory
+%[M,NumLines,NumElements,NumSamples] = readBinData('imageData_PlaneWave.bin');
 [M,NumLines,NumElements,NumSamples] = readBinData('imageData_Focused.bin');
 [ElementSpacing] = readTransducerParams();
 [BeamSpacing, fs, FocusR, FocusT, FNum, t0] = readImageParams();
 [c] = readConstants();
+fc = 4*10^6; % center frequency
 cd .. % go back to coding directory
 
 %% Image Generation Constants
@@ -19,7 +21,7 @@ a = 0.0015; % Gain value
 dt = 1/fs; % s
 dx = c*dt; % cm
 
-%% Custom Parameters
+%% Custom Beamforming Parameters
 FocusFractions = [1,2,3,4,5]*0.15; % where to focus rx beam
 rxFocus = (dx*NumSamples/2)*FocusFractions; % rxFocus positions in cm
 rxFocusSampleNumers = round(NumSamples*FocusFractions);
@@ -27,6 +29,9 @@ NumIncludedSamples = round(0.92*NumSamples); % cut at 92% of samples, only works
 
 %% Spatial Location Calculations
 [BeamLocations, ElementLocations, SampleLocations, SampleIndices] = SpatialLocator(BeamSpacing, NumLines, ElementSpacing, NumElements, dx, NumSamples,t0);
+
+%% Time Compensation
+M = M(t0:end,:,:);
 
 %% Delay and Sum Beamforming
 
@@ -47,7 +52,7 @@ end
 rfData = Combine_rx_Focus_data(rfData_MultiFoc,rxFocusSampleNumers);
 
 %% Bandpass Filter 
-Filt = bandpassfilter(rfData, fs/10 ,fs); % fc = fs/10
+Filt = bandpassfilter(rfData, fc ,fs);
 
 %% Time Gain Compensation ( gain as a function of time or depth)
 Filtamp = TGC(Filt, a);
@@ -61,10 +66,9 @@ A = exp(x*log(Envel));
 %% Ranges 
 lateralRange = NumLines*BeamSpacing;
 lateral = (-lateralRange/2:BeamSpacing:lateralRange/2)*10;
-
 axialRangetime = size(A,1)*dt;
 axialRangem = axialRangetime*c/2;
-axial  = (0:dt*c/2:axialRangem)*10; 
+axial  = (0:dt*c/2:axialRangem)*10;
 
 %% Image Formation 
 figure(1)
@@ -73,3 +77,7 @@ imagesc(lateral,axial,A) % colorRange)
 ylabel('Axial (mm)')
 xlabel('Lateral (mm)')
 axis image
+
+%% Plot Apodization Profile
+figure(2)
+plot(ApodizationProfile)
